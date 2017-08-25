46f4148290afcad9ad8dda358883218dbf636ca6
Tommy <tomludd@gmail.com>
2017-08-26 00:50:40 +0200

    Typo

--- src/TranslateFunction.js
+++ src/TranslateFunction.js
@@ -1,5 +1,5 @@
 ﻿window.jsl10n.translate = function (path) {
-    var n = window.jsl10n;
+    var node = window.jsl10n;
     path.split("/").forEach(function (element) {
         node = typeof node === "undefined" || node === null ? node : node[element];
     }, this);
