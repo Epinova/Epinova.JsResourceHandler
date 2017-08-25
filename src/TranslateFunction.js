b45368cea8df71493ae683dc7bda0a04dc299897
Tommy <tomludd@gmail.com>
2017-08-26 00:29:14 +0200

    Simplified, removed polyfill and adding function from js file.

--- src/TranslateFunction.js
+++ src/TranslateFunction.js
@@ -1,5 +1,8 @@
 ﻿window.jsl10n.translate = function (path) {
-    return path.split('/').reduce(function (o, x) {
-        return typeof o === "undefined" || o === null ? o : o[x];
-    }, window.jsl10n);
+    var n = window.jsl10n;
+    path.split("/").forEach(function (element) {
+        node = typeof node === "undefined" || node === null ? node : node[element];
+    }, this);
+
+    return node;
 };
